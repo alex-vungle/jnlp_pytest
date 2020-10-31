c05b82e4de18608af85757e087552135ab73b6ec
Alex Li <alex.li@vungle.com>
2020-11-01 07:35:48 +0800

    Add new test case

--- test_foo.py
+++ test_foo.py
@@ -1,14 +1,19 @@
 import pytest
 
 class Test_ABC:
-   # 测试类级开始
+
    def setup_class(self):
        print("------->setup_class")
-   # 测试类级结束
+
    def teardown_class(self):
        print("------->teardown_class")
+
    def test_a(self):
        print("------->test_a")
        assert 1
+
    def test_b(self):
        print("------->test_b")
+
+   def test_c(self):
+       print("------->test_c")
