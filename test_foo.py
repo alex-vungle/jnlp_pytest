8e4bb0abf0cd54ec666a36efce54fa1329677d8e
Alex Li <alex.li@vungle.com>
2020-11-01 11:59:20 +0800

    Update test case

--- test_foo.py
+++ test_foo.py
@@ -1,22 +1,38 @@
 import pytest
+import allure
 
+@allure.epic('My Epic')
+@allure.feature('My Feature')
 class Test_ABC:
 
-   def setup_class(self):
-       print("------->setup_class")
+    def setup_class(self):
+        print("------->setup_class")
 
-   def teardown_class(self):
-       print("------->teardown_class")
+    def teardown_class(self):
+        print("------->teardown_class")
 
-   def test_a(self):
-       print("------->test_a")
-       assert 1
+    @allure.story('Test A')
+    @allure.severity('blocker')
+    def test_a(self):
+        print("------->test_a")
+        assert 1
 
-   def test_b(self):
-       print("------->test_b")
+    @allure.story('Test B')
+    @allure.severity('major')
+    def test_b(self):
+        print("------->test_b")
 
-   def test_c(self):
-       print("------->test_c")
+    @allure.story('Test C')
+    @allure.severity('normal')
+    def test_c(self):
+        print("------->test_c")
 
-   def test_d(self):
-       print("------->test_d")
+    @allure.story('Test D')
+    @allure.severity('minor')
+    def test_d(self):
+        print("------->test_d")
+    
+    @allure.story('Test E')
+    @allure.severity('minor')
+    def test_e(self):
+        print("------->test_e")
